28b77e2e44c4d7486136a1ca0ba5988b856a8455
T30 <mateo.a.e8@gmail.com>
2023-12-14 14:34:11 -0500

    Creación usuario component

--- proyecto-prestamos/src/app/usuario/usuario.component.ts
+++ proyecto-prestamos/src/app/usuario/usuario.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-usuario',
   templateUrl: './usuario.component.html',
-  styleUrls: ['./usuario.component.css']
+  styleUrls: ['../app.component.css','./usuario.component.css']
 })
 export class UsuarioComponent {
 
