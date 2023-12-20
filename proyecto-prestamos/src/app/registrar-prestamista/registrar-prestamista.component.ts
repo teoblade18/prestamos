1900eeb0d34351a096b6eace24b405f61f70b2c1
T30 <mateo.a.e8@gmail.com>
2023-12-20 16:36:33 -0500

    Se añaden las validaciones del formControl

--- proyecto-prestamos/src/app/registrar-prestamista/registrar-prestamista.component.ts
+++ proyecto-prestamos/src/app/registrar-prestamista/registrar-prestamista.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import { NgModel } from '@angular/forms';
+import { NgModel, Validators } from '@angular/forms';
 import { Prestamista } from '../../Class/Prestamista';
+import { FormControl} from '@angular/forms';
+import { min } from 'rxjs';
 
 @Component({
   selector: 'app-registrar-prestamista',
@@ -8,6 +10,13 @@ import { Prestamista } from '../../Class/Prestamista';
   styleUrls: ['../app.component.css', './registrar-prestamista.component.css'],
 })
 export class RegistrarPrestamistaComponent {
+
+  nombreUsuarioFC = new FormControl('',Validators.required);
+  contraseniaFC = new FormControl('', [Validators.required, Validators.minLength(6)]);
+  nombreFC = new FormControl('', [Validators.required, Validators.pattern(/^[a-zA-ZÀ-ÿ\s]+$/)])
+  capitalFC = new FormControl('', [Validators.required, Validators.min(0)]);
+  numeroCuentaFC = new FormControl ('', [Validators.required, Validators.pattern(/^\d+$/)])
+
   prestamista: Prestamista = {
     nombre: '',
     capital: 0,
